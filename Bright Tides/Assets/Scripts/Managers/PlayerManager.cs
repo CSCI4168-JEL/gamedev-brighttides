705154f294fde19e239e25350d550708c996e711
jonathanignacio <jn721111@dal.ca>
2018-11-17 19:16:45 -0400

    add ship model and basic controlers/managers

---
+++ Bright Tides/Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour {
+
+    public static PlayerManager singleton;
+
+    [Header("Required Prefab References")]
+    [Tooltip("A reference to the PlayerCharacter prefab")]
+    public GameObject playerPrefab; // Should be assigned the player prefab
+
+    void Awake() {
+        bool execute = SetSingleton();
+
+        if (!execute) {
+            return;
+        }
+    }
+
+    bool SetSingleton() {
+        // Ensure no other instance exists
+        if (singleton == null) {
+            singleton = this;
+            DontDestroyOnLoad(gameObject);
+            return true;
+        }
+        else {
+            Destroy(gameObject);
+            return false;
+        }
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+    }
+
+    private void FixedUpdate() {
+    }
+}
