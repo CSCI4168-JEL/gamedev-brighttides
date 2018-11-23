3a78fc94986f0aa8aae2921fde41d532d5d9f1d7
jonathanignacio <jn721111@dal.ca>
2018-11-23 01:01:17 -0400

    change map generator to use text file input

---
+++ Bright Tides/Assets/Scripts/Managers/Tile Manager/MapGenerator.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    [Header("Map File")]
+    [Tooltip("The text file to read for a map. Must have valid dimensions.")]
+    public TextAsset mapFile;
+
+    private GameObject[,] tileMap; // Array of GameObjects (the map)
+    private int mapWidth;
+    private int mapHeight;
+
+    // Use this for initialization
+    void Start () {
+        string mapFileString = ParseTextFileToMapString(mapFile); // Validate and read the text file as string
+        int[,] parsedMapFile = ParseMapStringTo2DIntArray(mapFileString); // Parse the string into 2d int array
+
+        tileMap = GenerateTileMapFrom2DIntArray(parsedMapFile); // Generate the map and keep a reference
+    }
+
+    // Return parsed text asset as a string array, but throw an exception if it does not contain the required characters
+    private string ParseTextFileToMapString(TextAsset textFile) {
+        string parsedFile = textFile.ToString();
+
+        string playerSpawn = ((int)TileManager.TileTypes.playerSpawnTile).ToString();
+        string playerExit = ((int)TileManager.TileTypes.playerExitTile).ToString();
+
+        if (!parsedFile.Contains(playerSpawn) || !parsedFile.Contains(playerExit)) {
+            throw new System.Exception("Missing player spawn and/or exit!"); // Throw exception if the file is missing a player spawn and player exit
+        }
+
+        return parsedFile;
+    }
+
+
+    // Return 2D int array from map string, but throw an exception if it has inconsistent dimensions
+    private int[,] ParseMapStringTo2DIntArray(string mapString) {
+
+        string fileString = mapFile.ToString(); // Get the TextFile contents
+        fileString = fileString.Replace("\r", ""); // Remove carriage returns
+        string[] stringArray = fileString.Split('\n'); // Flatten map file into string array split by new line characters
+
+        // Intiially assume the height and width for the int array
+        mapHeight = stringArray.Length;
+        mapWidth = stringArray[0].Length;
+
+        int[,] intArray = new int[mapWidth, mapHeight];
+
+        int i = 0;
+        foreach (string line in stringArray) {
+            int j = 0;
+            if (line.Length != mapWidth) {
+                throw new System.Exception("String map dimensions must be rectangular. Current line width was " + line.Length + ", but expected width " + mapWidth); // Throw exception if the line is inconsistent width
+            }
+
+            foreach (char c in line) {
+                intArray[i, j] = (int)char.GetNumericValue(c); // Get the integer value of c
+
+                j++;
+            }
+            i++;
+        }
+
+        return intArray;
+    }
+
+    // The generator method that will produce the actual tile map from the 2D array
+    private GameObject[,] GenerateTileMapFrom2DIntArray(int[,] intArray) {
+        Vector3 tileSize = TileManager.singleton.tileSize; // The size of the tiles' bounds for uniform tile placement
+
+        GameObject[,] generatedTileMap = new GameObject[mapWidth, mapHeight]; // Instantiate the array with the proper size
+
+        for (int i = 0; i < mapWidth; i++) {
+            for (int j = 0; j < mapHeight; j++) {
+                Debug.Log(tileSize.ToString());
+                Vector3 tilePosition = new Vector3(tileSize.x * i, transform.position.y, tileSize.z * j); // Use the y value of the MapGenerator for tile height
+
+                TileManager.TileTypes currentTile = (TileManager.TileTypes)intArray[i, j]; // Cast it to the enum type for easier reference
+
+                switch(currentTile) {
+                    case TileManager.TileTypes.waterTile:
+                        TileManager.singleton.CreateWaterTile(tilePosition, Quaternion.AngleAxis(0, Vector3.up), transform);
+                        break;
+                    case TileManager.TileTypes.obstacleTile:
+                        TileManager.singleton.CreateIslandTile(tilePosition, Quaternion.AngleAxis(0, Vector3.up), transform);
+                        break;
+                }
+            }
+        }
+        return generatedTileMap;
+    }
+}
---
+++ Bright Tides/Assets/Scripts/Managers/Tile Manager/TileManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+
+    public static TileManager singleton; // The singleton instance
+
+    // Enum to store all tile types for map generation
+    public enum TileTypes : int {
+        playerSpawnTile,
+        playerExitTile,
+        waterTile,
+        obstacleTile,
+        enemySpawnTile,
+        treasureTile
+    }
+
+    [HideInInspector]
+    public Vector3 tileSize;
+
+    // Editor parameters, visible in inspector
+
+    [Header("Required Prefab References")]
+    [Tooltip("Water Tile")]
+    public GameObject waterTile;
+
+    [Tooltip("Obstacle Tiles")]
+    public List<GameObject> obstacleTiles;
+
+    void Awake() {
+        bool execute = SetSingleton();
+
+        if (!execute) {
+            return;
+        }
+    }
+
+    private void Start() {
+        tileSize = GetTileSize(); // Initialize the tile size
+    }
+
+    bool SetSingleton() {
+        // Ensure no other instance exists
+        if (singleton == null) {
+            singleton = this;
+            DontDestroyOnLoad(gameObject);
+            return true;
+        }
+        else {
+            Destroy(gameObject);
+            return false;
+        }
+    }
+
+
+    // Generate a temporary tile to get runtime size for uniform placement
+    private Vector3 GetTileSize() {
+        GameObject tile = CreateWaterTile(Vector3.zero, Quaternion.AngleAxis(0, Vector3.up), transform); // Generate temporary tile
+        Vector3 tileSize = tile.GetComponent<Renderer>().bounds.size;
+        GameObject.Destroy(tile);  // Get rid of temporary tile
+
+        return tileSize;
+    }
+
+    // Factory methods
+
+    // Water tile
+    public GameObject CreateWaterTile(Vector3 position, Quaternion rotation, Transform parent) {
+        return Instantiate(waterTile, position, Quaternion.AngleAxis(0, Vector3.up), parent);
+    }
+
+    // Obstacle Tile
+    public GameObject CreateIslandTile(Vector3 position, Quaternion rotation, Transform parent) {
+        GameObject selectedPrefab = obstacleTiles[Random.Range(0, obstacleTiles.Count - 1)];
+        return Instantiate(selectedPrefab, position, Quaternion.AngleAxis(0, Vector3.up), parent);
+    }
+}
