c2d7a34b3f5734df3d59f49fd1c63f56283190aa
Elliott <elliottadarbyshire@gmail.com>
2018-12-02 11:06:58 -0400

    Initial shop scene creation

---
+++ Bright Tides/Assets/ItemDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemDisplay : MonoBehaviour {
+
+    public Item item;
+
+    public Image itemIcon;
+    public Text itemName;
+    public Text purchasePrice;
+
+	// Use this for initialization
+	void Start () {
+        itemName.text = item.itemName;
+        itemIcon.sprite = item.icon;
+        purchasePrice.text = item.price.ToString();
+	}
+	
+	
+}
--- Bright Tides/Assets/Scripts/Scriptables/Item.cs
+++ Bright Tides/Assets/Scripts/Scriptables/Item.cs
@@ -2,8 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : ScriptableObject {
-	public GameObject self;
-	
-	
+[CreateAssetMenu(menuName = "Bright Tides/Item", fileName = "New Item", order = 1)]
+public class Item : ScriptableObject
+{
+    public EntityType entityType;
+
+    public string itemName;
+    public string description;
+    public int price;
+    public Sprite icon;
+
+    public int rangeModifier;
+    public int damageModifier;
+    public int speedModifier;
+    public int healthModifier;
+    public int maxHealthModifier;
+    public int ammoModifier;
+
 }
