355abb73986bdf3367334a6eed1976ced75bf9ac
jonathanignacio <jn721111@dal.ca>
2018-12-05 10:47:27 -0400

    Revert "update entity stats"
    This reverts commit 14cb50b20f875bbeafa3c1a5ea2ef6cfb3be7901.

--- Bright Tides/Assets/Scripts/Scriptables/EntityAttributes.cs
+++ Bright Tides/Assets/Scripts/Scriptables/EntityAttributes.cs
@@ -10,6 +10,9 @@ public class EntityAttributes : ScriptableObject {
 		
 	public string captainName;
 
+	public int level;
+	public int experience;
+
     public int maxHealth;
 	public int health;
 	public int ammo;
@@ -24,6 +27,12 @@ public class EntityAttributes : ScriptableObject {
 
 	public float baseAttackRange;
 	public int baseAttackDamage;
+
+	public int strength; // modifier for attack damage
+	public int agility; // modifier for taking damage reduction
+	public int dexterity; // modifier for successful hit
+	public int wisdom; // modifier for experience gain
+	public int luck; // modifier for critical damage, finding treasure, escaping combat, etc.
 
     public bool isPathableByPlayer; // If the entity occupies a tile, can it be pathed through by the player
     public bool isPathableByEnemy; // If the entity occupies a tile, can it be pathed through by the player
